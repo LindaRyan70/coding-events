2a62fdd523d896737ff2c15fc9b20573e65666c3
LRyan <lwryan6@gmail.com>
2022-10-03 01:06:38 -0500

    Completed Chptr 13.6.3 Coding Events Setup

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,26 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> theEvents = new ArrayList<>();
+        theEvents.add("Code With Pride");
+        theEvents.add("Strange Loop");
+        theEvents.add("Apple WWOC");
+        theEvents.add("SpringOne Platform");
+        model.addAttribute("events", theEvents);
+        return "events/index";
+    }
+
+}
