6929460dcb99e81f3d143bb37a74d1ebbd92d4f8
LRyan <lwryan6@gmail.com>
2022-10-05 23:26:44 -0500

    Created EventData class to manage and maintain ojects/event data  once created, refactored EventController methods accordingly.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +16,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    // Create new static field to hold/handle form data submitted. Use in createEvent() below.
+    // Created new static field List<String> to hold/handle form data submitted. Use in createEvent() below.
+
 //    private static List<String> events = new ArrayList<>();
-    private static List<Event> events = new ArrayList<>();
+
+/* No longer need this List<Event> data structure below b/c created a separate EventData class abstraction that
+encapsulates all the data-related behavior for Events in a Map<Integer, Event>  structure). */
+
+//    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+
+        // Refactor line below to update model value to call the getAll() static method on the EventData object.
+//        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -37,7 +46,10 @@ public class EventController {
     // Lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) { // variable MUST match form input name/value in html file
-        events.add(new Event(eventName, eventDescription));
+
+        //  Refactor line below to add a single event into its internal collection on the EventData object.
+        //  events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";  // redirects by default to root path for this controller (same as 'redirect:/events')
     }
 
