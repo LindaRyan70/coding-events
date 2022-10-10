840cca0681f37eaae64e02af0d4cb699fc02f88e
LRyan <lwryan6@gmail.com>
2022-10-10 13:26:32 -0500

    Chptr 15.2 Added  Validation Annotation in Model and View and created contact email field.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -7,12 +10,22 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+//   Chptr 15.2 - Added Validation Annotations for name, description, email. //
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Named must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description is too long!")
     private String description;
 
-    public Event(String name, String description) {
+//   Chptr 15.2 - Added contact email field, and added this to the constructor as well as getters/setters below.  //
+    @Email(message = "Invalid email. Try again!")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -31,6 +44,13 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+//   Chptr 15.2 - Added get/set for the new email filed above. //
+    public String getContactEmail() {return contactEmail;}
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
